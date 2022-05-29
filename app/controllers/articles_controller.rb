69c02c3f3f4c2271922828549aada668094b164b
Megan Van Der Weide <megan.vanderweide@gmail.com>
2022-05-30 09:45:26 +1000

    pre heroku upload

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,21 +4,46 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
 
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
         @article = Article.new
     end
 
     def create
-        @article = Article.new(title: "This is a title", body: "blah", importance: 9)
-       
+        @article = Article.new(article_params)
+
         if @article.save
-            redirect_to_articles_path
+            redirect_to articles_path
         else
             render :new
         end
     end
 
-    private 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to articles_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to home_path
+    end
+
+  private
+
     def article_params
         params.require(:article).permit(:title, :body, :importance)
     end
